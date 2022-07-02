544c97f0a15c030ed9faf0771fa3276c1b1a355c
Oni Olajumoke Priscilla <priscilla.oni@nash-renewables.com>
2022-07-02 17:55:05 +0100

    Add sidebar

--- pages/components/Sidebar.tsx
+++ pages/components/Sidebar.tsx
@@ -36,6 +36,8 @@ const musicMenu = [
     route: "/favorite",
   },
 ];
+
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`);
 const Sidebar = () => {
   return (
     <Box
@@ -45,7 +47,7 @@ const Sidebar = () => {
       paddingX="5px"
       color="gray"
     >
-      <Box paddingY="20px">
+      <Box paddingY="20px" height="100%">
         <Box width="120px" marginBottom="20px" paddingX="20px">
           <NextImage src="/logo.svg" height={60} width={120} />
         </Box>
@@ -91,6 +93,19 @@ const Sidebar = () => {
         </Box>
 
         <Divider color="gray.800" />
+        <Box height="60%" overflowY="auto" paddingY="20px">
+          <List spacing={2}>
+            {playlists.map((playlist) => (
+              <ListItem paddingX="20px" key={playlist}>
+                <LinkBox>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{playlist}</LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Box>
     </Box>
   );
