47a075cb4e323898b2f7d73ae8bc06eef008f3a2
Oni Olajumoke Priscilla <priscilla.oni@nash-renewables.com>
2022-07-02 13:31:02 +0100

    Add app structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,41 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import 'reset-css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import PlayerLayout from './components/playerLayout';
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#F5f5f5',
+      200: '#EEEEEE',
+      300: '#E0E0E0',
+      400: '#BDBDBD',
+      500: '#9E9E9E',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ':focus': {
+            outline: 'none',
+            boxShadow: 'none',
+          },
+        },
+      },
+    },
+  },
+})
+const  MyApp = ({ Component, pageProps }) => {
+  return ( 
+  <ChakraProvider>
+    <PlayerLayout>
+    <Component {...pageProps} />
+    </PlayerLayout>
+  </ChakraProvider>
+  ) 
 }
 
 export default MyApp
---
+++ pages/components/playerLayout.tsx
@@ -0,0 +1,21 @@
+import Sidebar from "./Sidebar";
+
+import { Box } from "@chakra-ui/layout";
+
+const playerLayout = ({ children }) => {
+  return (
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
+        <Sidebar />
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        {children}
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        player
+      </Box>
+    </Box>
+  );
+};
+
+export default playerLayout;
