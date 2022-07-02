261686734b7e91aac7b7adbd58c50fa5acaa5593
Oni Olajumoke Priscilla <priscilla.oni@nash-renewables.com>
2022-07-02 15:50:48 +0100

    Add top nav bar

---
+++ pages/components/Sidebar.tsx
@@ -0,0 +1,99 @@
+import NextImage from "next/image";
+import NextLink from "next/link";
+import {
+  Box,
+  List,
+  ListItem,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+  ListIcon,
+} from "@chakra-ui/layout";
+
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
+
+const navMenu = [
+  { name: "Home", icon: MdHome, route: "/" },
+  { name: "Search", icon: MdSearch, route: "/search" },
+  { name: "Your Library", icon: MdLibraryMusic, route: "/library" },
+];
+const musicMenu = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Favorites",
+    icon: MdFavorite,
+    route: "/favorite",
+  },
+];
+const Sidebar = () => {
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      <Box paddingY="20px">
+        <Box width="120px" marginBottom="20px" paddingX="20px">
+          <NextImage src="/logo.svg" height={60} width={120} />
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {navMenu.map((menu) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                <LinkBox>
+                  <NextLink href={menu.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menu.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menu.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box marginY="20px">
+          <List spacing={2}>
+            {musicMenu.map((music) => (
+              <ListItem paddingX="20px" fontSize="16px" key={music.name}>
+                <LinkBox>
+                  <NextLink href={music.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={music.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {music.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+
+        <Divider color="gray.800" />
+      </Box>
+    </Box>
+  );
+};
+
+export default Sidebar;
