ebbf08c851b9f7a1bc877f2826df4d5afd9e071b
Oni Olajumoke Priscilla <priscilla.oni@nash-renewables.com>
2022-07-05 07:59:54 +0100

    Add signup auth function

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,66 +1,64 @@
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcrypt'
 import { artistsData } from './songsData'
 
 const prisma = new PrismaClient()
 
-const run = async  () => {
-	await Promise.all(
-		artistsData.map(async (artist) => {
-		return prisma.artist.upsert({
-			where: { name: artist.name },
-			update: {},
-			create: {
-				name: artist.name,
-				songs: {
-					create: artist.songs.map(song => ({
-						name: song.name,
-						duration: song.duration,
-						url: song.url,
-					})),
-				},
-			},
-		})
-	})
-	)
+const run = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => ({
+              name: song.name,
+              duration: song.duration,
+              url: song.url,
+            })),
+          },
+        },
+      })
+    })
+  )
 
-	const salt = bcrypt.genSaltSync()
+  const salt = bcrypt.genSaltSync()
   const user = await prisma.user.upsert({
     where: { email: 'user@test.com' },
     update: {},
     create: {
       email: 'user@test.com',
       password: bcrypt.hashSync('password', salt),
-      firstName: 'Scott',
-      lastName: 'Moss',
     },
   })
 
-const songs = await prisma.song.findMany({})
-await Promise.all(
-	new Array(10).fill(1).map(async(_, i) => {
-		return prisma.playlist.create({
-			data: {
-				name: `Playlist #${i + 1}`,
-				user: {
-					connect: { id: user.id },
-				},
-				songs: {
-					connect: songs.map((song) => ({
-						id: song.id,
-					})),
-				},
-			},
-		})
-	})
-)
+  const songs = await prisma.song.findMany({})
+  await Promise.all(
+    new Array(10).fill(1).map(async (_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          user: {
+            connect: { id: user.id },
+          },
+          songs: {
+            connect: songs.map((song) => ({
+              id: song.id,
+            })),
+          },
+        },
+      })
+    })
+  )
 }
 
 run()
-.catch((e) => {
-	console.error(e)
-	process.exit(1) //1 means error 0 no error
-})
-.finally(async () => {
-	await prisma.$disconnect()
-})
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
