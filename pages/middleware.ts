5415d130fc2fbbf486a05c57241912e576656525
prolajumokeoni <prolajumokeoni@gmail.com>
2022-11-11 14:11:32 +0100

    update version

--- pages/middleware.ts
+++ pages/middleware.ts
@@ -6,7 +6,7 @@ export default function middleware(req) {
 	 if(signedinPages.find((p) => p === req.nextUrl.pathname)) {
 		const token = req.cookies.TRAX_ACCESS_TOKEN
 		if(!token) {
-			return NextResponse.redirect('/signin')
+			return NextResponse.rewrite('/signin')
 		}
 	 }
 
